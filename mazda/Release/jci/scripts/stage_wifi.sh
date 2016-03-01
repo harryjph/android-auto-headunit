cd456dbfe028b46dfd981184f760c9750efc3a08
Shailendra Padival <spadival@gmail.com>
2016-02-29 23:47:16 -0500

    V0.4 - Nav performance update & USB VIDs

--- mazda/Release/jci/scripts/stage_wifi.sh
+++ mazda/Release/jci/scripts/stage_wifi.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# AAserver start
-export LD_LIBRARY_PATH=/tmp/mnt/data_persist/dev/androidauto/custlib:/jci/lib:/jci/opera/3rdpartylibs/freetype:/usr/lib/imx-mm/audio-codec:/usr/lib/imx-mm/parser:/data_persist/dev/lib: 
-aaserver &
+# Android Auto start
+websocketd --port=9999 sh &
